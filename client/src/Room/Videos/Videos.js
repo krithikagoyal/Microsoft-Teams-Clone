adbe405d4175035f58eb353a0e9573c0accbd9bc
Krithika <goyalkrithika7@gmail.com>
2021-07-12 15:53:58 +0530

    double click pin added

--- client/src/Room/Videos/Videos.js
+++ client/src/Room/Videos/Videos.js
@@ -1,30 +1,41 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import './Videos.css';
 
 const Video = (props) => {
     const ref = useRef();
+    const ref2 = useRef();
 
     useEffect(() => {
         props.peer.on("stream", stream => {
             ref.current.srcObject = stream;
+            ref2.current.srcObject = stream;
         })
-    }, []);
+    }, [props.isUserPinned]);
 
     return (
-        <div className="video-container">
-            <video playsInline autoPlay ref={ref} className="display-video" />
-            <p className="username">{props.username}</p>
+        <div className={!props.isUserPinned.isPinned || (props.isUserPinned.peerID === props.peerID) ? "video-container" : "pinned-user"}>
+            <video onDoubleClick={() => props.handleDoubleClick(props.peerID)} playsInline autoPlay ref={ref} className={!props.isUserPinned.isPinned ? "display-video" : "pinned-user"} />
+            <p className={!props.isUserPinned.isPinned ? "username" : "pinned-user"}>{props.username}</p>
+            <video playsInline autoPlay ref={ref2} onDoubleClick={() => props.handleDoubleClick(props.peerID)} className={props.isUserPinned.isPinned && (props.isUserPinned.peerID === props.peerID) ? "display-video pinned" : "not-pinned"} />
+            <p className={props.isUserPinned.isPinned && (props.isUserPinned.peerID === props.peerID) ? "username" : "pinned-user"}>{props.username}</p>
         </div>
     );
 }
 
 
 function Videos(props) {
+    const [isUserPinned, pinStatus] = useState({ isPinned: false, peerID: null })
+
+    function handleDoubleClick(peerID) {
+        console.log(peerID)
+        pinStatus({ isPinned: !isUserPinned.isPinned, peerID: peerID })
+    }
+
     return (
         <div className="Container">
             {props.peers.map((peer) => {
                 return (
-                    <Video key={peer.peerID} peer={peer.peer} username={peer.username} />
+                    <Video peerID={peer.peerID} isUserPinned={isUserPinned} handleDoubleClick={handleDoubleClick} key={peer.peerID} peer={peer.peer} username={peer.username} />
                 );
             })}
         </div>
